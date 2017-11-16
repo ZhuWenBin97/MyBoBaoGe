8583041f0c9d28b1f43828316d8fa2e906d357e2
ZhuWB <911152556@qq.com>
2017-11-16 09:08:42 +0800

    通过服务器获取轮播图图片实现

---
+++ moudle_main/src/main/java/com/zhuwb/moudle_main/presenter/IAutoShufflingPresenter.java
@@ -0,0 +1,16 @@
+package com.zhuwb.moudle_main.presenter;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+
+import com.youth.banner.Banner;
+import com.zcy.hnkjxy.customview.AutoShufflingView;
+
+/**
+ * @author ZhuWB
+ *         创建时间 :2017/11/15 14:48
+ */
+
+public interface IAutoShufflingPresenter {
+    void getImages(FragmentActivity context,Banner banner);
+}
